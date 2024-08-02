f790642b2475bc2bbcc5df39b85e011201356085
SOS Nu <nhokanhanh@gmail.com>
2024-08-03 06:08:00 +0700

    'init'

---
+++ src/sosnujava/sosnu.java
@@ -0,0 +1,11 @@
+package sosnujava;
+
+public class sosnu {
+	public static void main(String[] args) {
+		System.out.println("Hello word sos nu with love");
+		System.out.println("Hello word sos nu with love");
+
+		System.out.println("Hello word sos nu with love");
+
+	}
+}
