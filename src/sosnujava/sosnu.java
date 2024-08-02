a50c9417e9c0f342a83b5325148c5c325f0cc4a7
SOS Nu <144415137+SOS-Nu@users.noreply.github.com>
2024-08-03 06:13:03 +0700

    Update sosnu.java

--- src/sosnujava/sosnu.java
+++ src/sosnujava/sosnu.java
@@ -4,7 +4,7 @@ public class sosnu {
 	public static void main(String[] args) {
 		System.out.println("Hello word sos nu with love");
 		System.out.println("Hello word sos nu with love");
-
+		System.out.println("Hello word sos nu with love");
 		System.out.println("Hello word sos nu with love");
 
 	}
